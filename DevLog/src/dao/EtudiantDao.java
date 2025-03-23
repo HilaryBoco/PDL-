e5bd7f20c05f8a12067f3450cbfcd5080e07dfdb
bocoh <bocoh@BHMA>
2025-03-23 08:09:42 +0100

    Il y a un souci de cosntructeur de fenetre et de variables au niveau de consulter étudiant

--- DevLog/src/dao/EtudiantDao.java
+++ DevLog/src/dao/EtudiantDao.java
@@ -2,9 +2,10 @@ package dao;
 import model.*;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class EtudiantDao extends ConnectionDao{
-	
+	private ArrayList<Student> listEtudiant;
 	public EtudiantDao() {
 		super();
 	}
@@ -51,8 +52,104 @@ public class EtudiantDao extends ConnectionDao{
 			}catch(Exception ignore) {
 			}
 		}
-			
 		return returnValue;
 	}
-	
+		
+		public ArrayList<Student>getAll() {
+			
+			
+			listEtudiant = new ArrayList<>();
+			Connection con = null;
+			PreparedStatement ps = null;
+			ResultSet rs = null;
+
+			try {
+				con = DriverManager.getConnection(URL, LOGIN, PASS);
+				ps = con.prepareStatement("SELECT * FROM STUDENT ORDER BY IDETUDIANT");
+				rs = ps.executeQuery();
+				
+				while(rs.next()){
+					listEtudiant.add(new Student(rs.getInt("IDETUDIANT"), rs.getString("NOM"), rs.getString("PRENOM"),
+														rs.getString("DATEDENAISSANCE"), rs.getInt("CLASSEMENT"), null, rs.getInt("PROMOTION"), rs.getInt("FILIERE")));
+				}
+				
+				
+			}
+			catch(Exception e) {
+				if(e.getMessage().contains("ORA-00001"))
+					System.out.println("L’étudiant existe déjà dans la base de données");
+				else
+					e.printStackTrace();
+			}
+			finally {
+				
+				try {
+					if(rs != null) {
+						rs.close();
+					}	
+				}catch(Exception ignore) {
+				}
+				
+				
+				
+				
+				try {
+					if(ps != null) {
+						ps.close();
+					}	
+				}catch(Exception ignore) {
+				}
+				try {
+					if(con != null) {
+						con.close();
+					}
+				}catch(Exception ignore) {
+				}
+			}
+				
+			return listEtudiant;
+		}
+		
+		
+		public int update(Student etu) {
+			
+			Connection con = null;
+			PreparedStatement ps = null;
+			int returnValue = 0;
+			
+			try {
+				con = DriverManager.getConnection(URL, LOGIN, PASS);
+				ps = con.prepareStatement("UPDATE STUDENT SET NOM = ?, PRENOM = ?, DATEDENAISSANCE = ?, CLASSEMENT = ? , PROMOTION = ? , FILIERE = ? WHERE IDETUDIANT = ?");
+				ps.setString(1, etu.getNom());
+				ps.setString(2, etu.getPrenom());
+				ps.setString(3,etu.getDateDeNaissance());
+				ps.setInt(4, etu.getClassement ());
+				ps.setInt(5, etu.getPromotion ());
+				ps.setInt(6, etu.getfiliere());
+				ps.setInt(7, etu.getId());
+			
+				returnValue = ps.executeUpdate();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if(con != null) {
+						con.close();
+					}
+				}catch(Exception ignore) {
+				}
+				
+				try {
+					if(ps != null) {
+						ps.close();
+					}
+				}catch(Exception ignore) {
+				}
+				
+			}
+			
+			return returnValue;
+		}
+		
 }
